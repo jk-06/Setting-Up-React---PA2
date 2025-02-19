f60e49f6dccfbe35ab7ac1d2f396c019bf1db771
Jebakumar <jebakumar.6208@gmail.com>
2025-02-19 10:14:19 +0530

    completed

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Jebakumar</h1>
+            <p>Contact Details: jebakumar@gmail.com</p>
+        </div>
+    );
+};
+
+export default Home;
